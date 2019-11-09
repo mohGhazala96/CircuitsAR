23d931170a1b6d7ada260d00e622db2c4bd88632
MayaOsama <35902575+MayaOsama@users.noreply.github.com>
2019-11-09 18:08:56 +0200

    fixing multiple popup menus

--- CircuitsAR/Assets/Scripts/UIManager.cs
+++ CircuitsAR/Assets/Scripts/UIManager.cs
@@ -6,11 +6,25 @@ public class UIManager : MonoBehaviour
 {
     public GameObject uiElement;
     public GameObject canvas;
-
+    static bool isBatteryMenuOpen;
+    static bool isResistorMenuOpen;
+    static bool isBuzzerMenuOpen;
+    static bool isLedMenuOpen;
+    static public bool instantiate;
+    public bool closed;
+    private void Start()
+    {
+        closed = false;
+        isBatteryMenuOpen = false;
+        isResistorMenuOpen = false;
+        isBuzzerMenuOpen = false;
+        isLedMenuOpen = false;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.GetMouseButtonDown(0) )
         {
 
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -20,12 +34,46 @@ public class UIManager : MonoBehaviour
                 var selection = hit.transform;
                 if (!selection.CompareTag("Circuit"))
                 {
-                    GameObject currrentUiElement = Instantiate(uiElement, canvas.transform);
-                    UIElement uielement = currrentUiElement.GetComponent<UIElement>();
-                    uielement.element = selection.gameObject;
+                    if ((selection.CompareTag("Buzzer") && !isBuzzerMenuOpen) || (selection.CompareTag("Led") && !isLedMenuOpen)
+                        || (selection.CompareTag("Resistor") && !isResistorMenuOpen)|| (selection.CompareTag("Battery") && !isBatteryMenuOpen))
+                    {
+                        GameObject currrentUiElement = Instantiate(uiElement, canvas.transform);
+                        UIElement uielement = currrentUiElement.GetComponent<UIElement>();
+                        uielement.element = selection.gameObject;
+                        if(selection.CompareTag("Buzzer"))
+                            isBuzzerMenuOpen = true;
+                        if (selection.CompareTag("Led"))
+                            isLedMenuOpen = true;
+                        if (selection.CompareTag("Resistor"))
+                            isResistorMenuOpen = true;
+                        if (selection.CompareTag("Battery"))
+                            isBatteryMenuOpen = true;
+                    }
+
                 }
                 
             }
         }
+
+        GameObject[] uiElements = GameObject.FindGameObjectsWithTag("UIElement");
+
+        isBuzzerMenuOpen = false;
+        isBatteryMenuOpen = false;
+        isResistorMenuOpen = false;
+        isLedMenuOpen = false;
+
+        foreach (GameObject uiElement in uiElements)
+        {
+            if (uiElement.GetComponent<UIElement>().element.CompareTag("Buzzer"))
+                isBuzzerMenuOpen = true;
+            if (uiElement.GetComponent<UIElement>().element.CompareTag("Led"))
+                isLedMenuOpen = true;
+            if (uiElement.GetComponent<UIElement>().element.CompareTag("Resistor"))
+                isResistorMenuOpen = true;
+            if (uiElement.GetComponent<UIElement>().element.CompareTag("Battery"))
+                isBatteryMenuOpen = true;
+        }
+
+
     }
 }
